067d89abc47a81f54462051fdb85ccfcb8501934
*vivek <vrvivekofficial@gmail.com>
2024-05-28 16:49:23 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 import './App.css'
-import { Navbar, } from "./components";
+import { Navbar,Exchanges,Cryptocurrencies,CryptoDetails,News } from "./components";
 
 const App = () => {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,16 @@
 import { default as Navbar } from "./Navbar/Navbar";
+import { default as HomePage } from "./HomePage/HomePage";
+import { default as Exchanges } from "./Exchanges/Exchanges"
+import { default as Cryptocurrencies } from "./Cryptocurrencies/Cryptocurrencies";
+import { default as CryptoDetails } from "./CryptoDetails/CryptoDetails";
+import { default as News } from "./News/News";
 
 export {
-  Navbar
+  Navbar,
+  HomePage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News
 }
 
