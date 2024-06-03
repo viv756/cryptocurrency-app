3a571bb0e828a3788209d7c13ebb61de723a0db8
*vivek <vrvivekofficial@gmail.com>
2024-06-03 16:14:28 +0530

    search field added

--- src/components/Cryptocurrencies/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies/Cryptocurrencies.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 
-import { useGetCryptosQuery } from '../../services/cryptoApi';
-import { useEffect } from 'react';
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+import { useEffect } from "react";
 
-const Cryptocurrencies = ({simplified}) => {
-  const count = simplified ? 10 : 100
-  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-  const [cryptos, setCryptos] = useState([])
-  const [searchTerm, setSearchTerm] = useState('');
-  
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
-    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
-    
-    setCryptos(filteredData)
-},[cryptosList,searchTerm])
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
   if (isFetching) {
-    return 'Loading...'
+    return "Loading...";
   }
-  
+
   return (
     <>
-        <div className="search-crypto">
-        <Input placeholder="search Cryptocurrency" onChange={(e)=>setSearchTerm(e.target.value)}></Input>
-
+      <div className="search-crypto">
+        <Input placeholder="search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}></Input>
       </div>
-      <Row gutter={[32, 32]} className='crypto-card-container'>
+      <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
-              <Card title={`${currency.rank}.${currency.name}`}
+              <Card
+                title={`${currency.rank}.${currency.name}`}
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
-                hoverable
-              >
+                hoverable>
                 <p>Price:{millify(currency.price)}</p>
                 <p>Market Cap:{millify(currency.marketCap)}</p>
-                <p>Price:{millify(currency.change) }</p>
+                <p>Price:{millify(currency.change)}</p>
               </Card>
             </Link>
           </Col>
         ))}
-        
-    </Row>
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
