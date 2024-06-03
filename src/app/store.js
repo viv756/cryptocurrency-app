dcac7d21a5da8437a5e5cdfba81e7e7511b2ad59
*vivek <vrvivekofficial@gmail.com>
2024-06-03 15:19:15 +0530

    global stats added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-export default configureStore({
+const store =configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
+
+setupListeners(store.dispatch);
+
+export default store;
