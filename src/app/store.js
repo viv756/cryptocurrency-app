2020268ff7f387d4e3f6d84248b0ba7af13117e1
*vivek <vrvivekofficial@gmail.com>
2024-06-03 14:47:30 +0530

    redux store created

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,6 @@ import { cryptoApi } from "../services/cryptoApi";
 
 export default configureStore({
   reducer: {
-    [cryptoApi.reducerPath]:cryptoApi.reducer
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
   },
-})
+});
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,21 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  'X-RapidAPI-Key': 'ad905b7620msh4160c6227a9ae3fp1eb987jsn50bab84549f2',
-  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-}
+  "X-RapidAPI-Key": "ad905b7620msh4160c6227a9ae3fp1eb987jsn50bab84549f2",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = "https://coinranking1.p.rapidapi.com/coins";
 
-const createRequest = (url)=>({url, headers:cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-  reducerPath: 'cryptoApi',
+  reducerPath: "cryptoApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query:()=>createRequest('/exchanges')
-    })
-  })
-})
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
 
+export const { useGetCryptosQuery } = cryptoApi;
