860d8c80116195d856f11a12633d8247113dadbd
*vivek <vrvivekofficial@gmail.com>
2024-06-04 10:43:01 +0530

    news section modified

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card, Input } from "antd";
 import moment from "moment";
 import { useGetCryptoNewsQuery } from "../../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -9,12 +10,27 @@ const { Option } = Select;
 const deomimageUrl = "http://coinrevolution.com/ep-content/uploads/2020/06/cryptonews.jpg";
 
 const News = ({ simplified }) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 6 : 12 });
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const { data } = useGetCryptosQuery(100);
 
   if (!cryptoNews?.value) return "Loading...";
 
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="select a crypto"
+            optionFilterProp="children"
+            onChange={(value) => console.log(value)}
+            filterOption={(input, option) => option.children.toLoweCase().indexOf(input.toLowerCase()) >= 0}>
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
